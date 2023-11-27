5877f302f9112df077e684381e4e6b775a5e2afb
Rodrigo Amaral Jucá <113607491+gtamaral@users.noreply.github.com>
2023-11-27 11:35:05 -0300

    teste array is array

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -44,7 +44,7 @@ const Home = () => {
 
   return (
     <div id='home'>
-      {photos && photos.map((photo) => 
+      {Array.isArray(photos) && photos.map((photo) => 
         (
           <div key={photo._id}>
             <PhotoItem photo={photo} />
